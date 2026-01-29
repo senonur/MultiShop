173f9500831c065aba1564a604a5dee171d61817
Onur ŞEN <onursen_1993@hotmail.com>
2026-01-30 00:58:13 +0300

    Identity server çalışmaları

---
+++ IdentityServer/MultiShop.IdentityServer/Controllers/RegisterController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.IdentityServer.Dtos;
+using MultiShop.IdentityServer.Models;
+using System.Threading.Tasks;
+
+namespace MultiShop.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegisterController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public RegisterController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> UserRegister(UserRegisterDto userRegisterDto)
+        {
+            var values = new ApplicationUser()
+            {
+                Email = userRegisterDto.Email,
+                UserName = userRegisterDto.UserName,
+                Name = userRegisterDto.Name,
+                Surname = userRegisterDto.Surname,
+            };
+            var result = await _userManager.CreateAsync(values, userRegisterDto.Password);
+            if (result.Succeeded)
+            {
+
+                return Ok("Kullanıcı eklendi");
+
+
+            }
+            else
+            {
+                return Ok("Hata oluştu tekrar deneyiniz");
+            }
+
+
+
+        }
+    }
+}
